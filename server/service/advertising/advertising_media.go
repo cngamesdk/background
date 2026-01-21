7a7c527f9647be0ae3aa5780a6690fd16ba75d9f
tangping <442353346@qq.com>
2026-01-21 10:47:58 +0800

    A 媒体列表增加维度显示

--- server/service/advertising/advertising_media.go
+++ server/service/advertising/advertising_media.go
@@ -3,28 +3,34 @@ package advertising
 import (
 	"context"
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
+	model2 "github.com/flipped-aurora/gin-vue-admin/server/model"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/advertising"
 	api2 "github.com/flipped-aurora/gin-vue-admin/server/model/advertising/api"
 	"go.uber.org/zap"
 )
 
 type AdvertisingMediaService struct {
-	
 }
 
 func (receiver *AdvertisingMediaService) List(ctx context.Context, req *api2.AdvertisingMediaListReq) (resp interface{}, total int64, err error) {
+	alias := "ad"
 	model := advertising.NewDimAdvertisingMediaModel()
-	tmpDb := model.Db().WithContext(ctx).Table(model.TableName())
+	tmpDb := model.Db().WithContext(ctx).Table(model.TableName() + " as " + alias)
 	if req.AdvertisingMediaName != "" {
-		tmpDb.Where("id = ? or advertising_media_name like ?", req.AdvertisingMediaName, "%"+ req.AdvertisingMediaName +"%")
+		tmpDb.Where("id = ? or advertising_media_name like ?", req.AdvertisingMediaName, "%"+req.AdvertisingMediaName+"%")
 	}
 	if countErr := tmpDb.Count(&total).Error; countErr != nil {
 		err = countErr
 		global.GVA_LOG.Error("获取总数异常", zap.Error(countErr))
 		return
 	}
+	model2.JoinPlatform(tmpDb, alias)
 	var list []advertising.DimAdvertisingMediaModel
-	if listErr := tmpDb.Limit(req.PageSize).Offset((req.Page - 1)*req.PageSize).Find(&list).Error; listErr != nil {
+	if listErr := tmpDb.
+		Select(alias + ".*,platform_name").
+		Limit(req.PageSize).
+		Offset((req.Page - 1) * req.PageSize).
+		Find(&list).Error; listErr != nil {
 		err = listErr
 		global.GVA_LOG.Error("获取异常", zap.Error(listErr))
 		return
