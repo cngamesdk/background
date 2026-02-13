6bce5ad081f88bc78eaca5660b5d666b4d286a66
tangping <442353346@qq.com>
2026-02-13 12:06:39 +0800

    A 增加媒体授权state进行加密

--- server/model/advertising/api/advertising_auth.go
+++ server/model/advertising/api/advertising_auth.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"encoding/json"
 	error2 "github.com/cngamesdk/go-core/model/error"
+	"github.com/duke-git/lancet/v2/cryptor"
+	"github.com/flipped-aurora/gin-vue-admin/server/global"
 	"github.com/flipped-aurora/gin-vue-admin/server/model/advertising"
 	"github.com/flipped-aurora/gin-vue-admin/server/utils"
 	"github.com/gin-gonic/gin"
 	errors2 "github.com/pkg/errors"
+	"net/url"
 )
 
 type AdvertisingAuthRedirectReq struct {
@@ -42,7 +45,8 @@ func (receiver *AdvertisingAuthRedirectReq) Validate(ctx context.Context) (err e
 		err = errors2.Wrap(stateDataErr, "JSON state异常")
 		return
 	}
-	receiver.State = string(stateDataByte)
+	cryptStateData := cryptor.AesEcbEncrypt(stateDataByte, []byte(global.GVA_CONFIG.Common.AesCryptKey))
+	receiver.State = url.QueryEscape(string(cryptStateData))
 
 	model := advertising.NewDimAdvertisingDeveloperConfigModel()
 	if takeErr := model.Take(ctx, "*", "id = ?", receiver.Id); takeErr != nil {
